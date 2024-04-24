edfc9adbfdb24584a09eb56afd8c1b1964a91475
Sarah A <sarah4@kth.se>
2024-04-24 15:59:40 +0200

    add working route _monitor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+import express from "express";
 import {Router, Request, Response} from "express";
 
+const app = express();
+const port = 3000;
+
+app.listen(port, () => console.log("It is working!"));
+
 const router = Router();
+
+app.use("/canvas-kth-sandboxes", router);
 
 router.get("/_monitor", monitor);
 
