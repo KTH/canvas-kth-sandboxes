cb616e4cd274e9acccf1cd5d181fe9201adb7b4c
Sarah A <sarah4@kth.se>
2024-04-29 13:24:49 +0200

    add logging and clean up package.json

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
+import "./config/start";
 import express from "express";
 import { Router, Request, Response } from "express";
+import log from "skog";
 
 const app = express();
 const port = 3000;
 
-app.listen(port, () => console.log("Sandbox app up and running"));
+app.listen(port, () => log.info("Sandbox app up and running"));
 
 const router = Router();
 
@@ -12,10 +14,11 @@ app.use("/canvas-kth-sandboxes", router);
 
 router.get("/_monitor", monitor);
 
-async function monitor(req: Request, res: Response) {
+async function monitor(req: Request, res: Response) {  
   try {
     res.send("OK");
   } catch (error) {
     res.send("Application status: ERROR");
+    log.error("Error: something went wrong.")
   }
 }
